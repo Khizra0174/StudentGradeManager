5bd83fd2ab49ef9d79befa69d0910c18bbacbb84
L1F22BSSE0023 <L1F22BSSE0023@ucp.com>
2025-06-19 14:09:32 +0500

    Made Student class with basic getter and setter only

--- StudentGradeManager/src/main/java/studentgrademanager/Students.java
+++ StudentGradeManager/src/main/java/studentgrademanager/Students.java
@@ -1,5 +1,35 @@
 package studentgrademanager;
 
-public class Students {
+import java.util.ArrayList;
 
+public class Students {
+	private String name;
+	private String id;
+	private String grade;
+	public Students(String id,String name,String grade) {
+		this.id = id;
+		this.name=name;
+		this.grade= grade;
+	}
+	public void setName(String name) {
+		this.name= name;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public String getId() {
+		return this.id;
+	}
+	public String getGrade() {
+		return this.grade;
+	}
+	public void calculateGrade() {
+		
+	}
 }
